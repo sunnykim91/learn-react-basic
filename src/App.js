7b66c89782d8275d5a256051a0eb926aa9fa4619
unknown <tjsgnl2002@gmail.com>
2019-08-22 21:31:29 +0900

    fix somethigs

--- src/App.js
+++ src/App.js
@@ -2,49 +2,51 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie';
 
-const movies = [
-  {
-    title: "Matrix",
-    poster: "https://t1.daumcdn.net/movie/e49c2e4eb419a9813228b5ab6bc5b039362236ea"
-  },
-  {
-    title: "Full Metal Jacket",
-    poster: "http://thumbnail.egloos.net/600x0/http://pds25.egloos.com/pds/201507/13/40/b0113440_55a2ef988b4f8.jpg"
-  },
-  {
-    title: "Oldboy",
-    poster: "https://t1.daumcdn.net/thumb/R720x0/?fname=http://t1.daumcdn.net/brunch/service/user/6rIc/image/gCQXUtxPRSKlcl8QbUzOIUtLpxI"
-  },
-  {
-    title: "Star Wars",
-    poster: "https://t1.daumcdn.net/cfile/tistory/223BE445567253F028"
-  }
-]
+
 
 class App extends Component {
 
+    
+
   state = {
-    greeting: 'hello!'
   }
 
   componentDidMount() {
-    // setTimeout(() => {
-    //   this.setState({
-    //     greeting: 'Hello again!'
-    //   })
-    // }, 5000)
-    this.setState({
-      greeting: 'Hello again!'
+    setTimeout(() => {
+      this.setState({
+        movies: [
+          {
+            title: "Matrix",
+            poster: "https://t1.daumcdn.net/movie/e49c2e4eb419a9813228b5ab6bc5b039362236ea"
+          },
+          {
+            title: "Full Metal Jacket",
+            poster: "http://thumbnail.egloos.net/600x0/http://pds25.egloos.com/pds/201507/13/40/b0113440_55a2ef988b4f8.jpg"
+          },
+          {
+            title: "Oldboy",
+            poster: "https://t1.daumcdn.net/thumb/R720x0/?fname=http://t1.daumcdn.net/brunch/service/user/6rIc/image/gCQXUtxPRSKlcl8QbUzOIUtLpxI"
+          },
+          {
+            title: "Star Wars",
+            poster: "https://t1.daumcdn.net/cfile/tistory/223BE445567253F028"
+          }
+        ]
+      })
+    }, 3000);
+  }
+
+  _renderMovies = () => {
+    const movies = this.state.movies.map((movie, index) => {
+      return <Movie title={movie.title} poster={movie.poster} key={index} />
     })
+    return movies
   }
 
   render() {
     return (
       <div className="App">
-        {this.state.greeting}
-        {movies.map( (movie, index) => {
-          return <Movie title={movie.title} poster={movie.poster} key={index} />
-        })}
+        {this.state.movies ? this._renderMovies() : 'Loading'}
       </div>
     );
   }
